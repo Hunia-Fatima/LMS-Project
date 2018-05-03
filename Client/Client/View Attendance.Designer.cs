54d05115f291b59e27c69201b6846046c02f5f6d
Fatima-Khalil-Butt <fatimakhalilahmedbutt@gmail.com>
2018-05-03 11:44:22 +0500

    I added view of attendance by registered student.

---
+++ Client/Client/View Attendance.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CLient
+{
+    partial class View_Attendance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linklabel = new System.Windows.Forms.LinkLabel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linklabel
+            // 
+            this.linklabel.AutoSize = true;
+            this.linklabel.Location = new System.Drawing.Point(263, 222);
+            this.linklabel.Name = "linklabel";
+            this.linklabel.Size = new System.Drawing.Size(54, 13);
+            this.linklabel.TabIndex = 3;
+            this.linklabel.TabStop = true;
+            this.linklabel.Text = "GO BACK";
+            this.linklabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linklabel_LinkClicked);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-33, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(494, 176);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // View_Attendance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 342);
+            this.Controls.Add(this.linklabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "View_Attendance";
+            this.Text = "View_Attendance";
+            this.Load += new System.EventHandler(this.View_Attendance_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linklabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Client/Client/View Attendance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLient
+{
+    public partial class View_Attendance : Form
+    {
+        public View_Attendance()
+        {
+            InitializeComponent();
+            BindingSource source = new BindingSource();
+            server.Service1 server = new server.Service1();
+            source.DataSource = server.ViewAttendance(server.loggedInStudent());
+           dataGridView1.DataSource = source;
+        }
+
+        private void View_Attendance_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linklabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            Student form = new Student();
+            this.Hide();
+            form.Show();
+
+        }
+    }
+}
